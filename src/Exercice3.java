c4a60bd714c7ff4f277ddf8e8d607fe52d327d3a
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-05-31 10:35:03 +0200

    Amélioration de l'exercice 3

--- src/Exercice3.java
+++ src/Exercice3.java
@@ -1,25 +1,28 @@
+
+/**
+ *
+ * @author Maxime BLAISE
+ * @version 1.0
+ */
 public class Exercice3 {
 
-	/**
-	 * Méthode principale
-	 * 
-	 * @param args les arguments
-	 */
-	public static void main(String[] args) {
-		// Création de trois nombres flottants
-		float x = (float)0.3;
-		float y = (float)0.6;
-		float z = (float)0.9;
-
-		System.out.println("x="+x);
-		System.out.println("y="+y);
-		System.out.println("z="+z);
-
-		// Ecrit bonjour si troisième est égal à la somme des deux premiers
-		if(z == (y + x)) {
-			System.out.println("Bonjour");
-		} else {
-			System.out.println("PAS BONJOUR !");
-		}
-	}
+    /**
+     * Méthode principale.
+     *
+     * @param args les arguments
+     */
+    public static void main(String[] args) {
+        // Création de trois nombres flottants
+        float x = (float) 0.3;
+        float y = (float) 0.6;
+        float z = (float) 0.9;
+        // Affichage des nombres
+        System.out.println("x=" + x);
+        System.out.println("y=" + y);
+        System.out.println("z=" + z);
+        // Ecrit bonjour si troisième est égal à la somme des deux premiers
+        if (z == (y + x)) {
+            System.out.println("Bonjour");
+        }
+    }
 }
