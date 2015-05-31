51d82fc3b06a320eed5234204efd04c317aedd86
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-05-31 10:35:10 +0200

    Amélioration de l'exercice 4

--- src/Exercice4.java
+++ src/Exercice4.java
@@ -1,9 +1,38 @@
+
+/**
+ * 
+ * @author Maxime BLAISE
+ * @version 1.0
+ */
 public class Exercice4 {
 
-	public static void main(String[] args) {
-		boolean condition = true;
-		if(condition = false) {
-			System.out.println("Huh!");
-		}
-	}
+    /**
+     * Méthode principale.
+     *
+     * @param args Les arguments
+     */
+    public static void main(String[] args) {
+        premierCas();
+        deuxiemeCas();
+    }
+
+    /**
+     * Premier cas énoncé par le sujet.
+     */
+    public static void premierCas() {
+        boolean condition = false;
+        if (condition = true) {
+            System.out.println("Huh!");
+        }
+    }
+
+    /**
+     * Inversement.
+     */
+    public static void deuxiemeCas() {
+        boolean condition = true;
+        if (condition = false) {
+            System.out.println("Huh!");
+        }
+    }
 }
