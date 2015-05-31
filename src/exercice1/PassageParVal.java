1e2842a81b2758e20c253f15a1e62b367f1acc77
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-05-31 10:13:15 +0200

    Amélioration de l'exercice 1
    Eclaircissement du code.

---
+++ src/exercice1/PassageParVal.java
@@ -0,0 +1,36 @@
+package exercice1;
+
+/**
+ * @author Maxime BLAISE
+ * @version 1.0
+ */
+public class PassageParVal {
+
+    /**
+     * Méthode principale.
+     *
+     * @param args Les arguments du programme
+     */
+    public static void main(String[] args) {
+        // Création d'une constante double
+        final double val = 5.0;
+        // Affichage
+        System.out.println("Valeur initiale : " + val);
+        // Division
+        double nouvelleVal = diviser(val);
+    }
+
+    /**
+     * Méthode diviser, qui divise la valeur par deux, l'affiche et la retourne.
+     *
+     * @param valeur la valeur à diviser
+     * @return la valeur finale divisée par deux
+     */
+    public static double diviser(double valeur) {
+        // Division de la valeur
+        double res = valeur /= 2;
+        // Affichage
+        System.out.println("Valeur après division : " + res);
+        return res;
+    }
+}
